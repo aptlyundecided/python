6f946895012cf0b3e672ee23a88622e594918568
Alex Wilson <adwilson0286@gmail.com>
2017-06-02 21:23:08 -0400

    :art: first!

---
+++ src/first.py
@@ -0,0 +1,20 @@
+# My first python program.
+"""I am the first python program authored by Alex Wilson"""
+#
+# Define a function.
+#
+def tester():
+    #
+    # Docstring?
+    #
+    """Does this work?"""
+    #
+    # Let's do some tootin'
+    #
+    farts = "Farts!"
+    print(farts)
+    print("Poop-a-loop")
+#
+# Run the function
+#
+tester()
