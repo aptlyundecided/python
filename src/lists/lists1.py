b7861db4393221292bdedcf1199f819408ca1523
Alex Wilson <adwilson0286@gmail.com>
2017-06-09 20:47:26 -0400

    :construction: bloggin'

---
+++ src/lists/lists1.py
@@ -0,0 +1,11 @@
+# Let's learn about lists.
+"""Let's learn a little bit about lists"""
+#
+# Define a list
+#
+FIRST = [1, 2, 3, 4, 5]
+#indirect_reference = first_list[2]
+#
+# Should === 3
+#
+print(FIRST.length)
