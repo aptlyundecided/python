ac2934916da072157cf25e2fe5aecf181121e615
Alex Wilson <adwilson0286@gmail.com>
2017-06-21 21:35:34 -0400

    :construction:

--- src/challenges/1/hi_temp_message.py
+++ src/challenges/1/hi_temp_message.py
@@ -1,2 +1,12 @@
 """Send a notification about record high temperature"""
+
+"""
+You can make this file do whatever you want.  But this file is where you
+give your server real power.  For the sake of simplicity, all it's doing
+is printing a stupid message.
+
+We will give the server real powers soon!
+
+Until then...
+"""
 print('Record high temps achieved yesterday.')
