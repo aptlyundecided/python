d235817afe49fc09352c779eb6c64c2c1a7d96ff
Alex Wilson <adwilson0286@gmail.com>
2017-06-30 17:48:13 -0400

    :construction: Challenge 3

---
+++ src/challenges/3/tests.py
@@ -0,0 +1,16 @@
+"""Unit tests with Python"""
+import pytest
+import datetime
+
+def what_day():
+    """Return an integer, representing the day of the week"""
+
+    # 0 - 6 [Mon - Sun]
+    return datetime.datetime.today().weekday()
+
+
+def test_answer():
+    """unit test for what_day - expects 4"""
+    assert what_day() == 4
+
+print(test_answer())
