1f2420ad92e5aa6e8ec31dcc5c15d316fa4598d6
Alex Wilson <adwilson0286@gmail.com>
2017-07-13 21:33:38 -0400

    :constsruction: challenge number 4!

--- challenges/4/pandas01.py
+++ challenges/4/pandas01.py
@@ -2,4 +2,50 @@
 First mess around with pandas
 """
 
-import pandas as pd
+
+# Import tool(s)
+import pandas
+
+# Create a dataframe straight from a .csv file
+CARS = pandas.read_csv('assets/cars.csv', sep=';')
+
+# Get the names of each of the columns in the data frame
+print(list(CARS))
+
+# Create a list of the MPG's without the first column?  IS it because the
+# the first column is the name of the column?
+CARS['MPG'] = CARS['MPG'].iloc[1:].astype(float)
+
+# Create a zero filter
+NON_ZERO_FILTER = CARS['MPG'] != 0
+
+# Filtered MPG
+MPG = CARS[NON_ZERO_FILTER]
+
+# Get all those data points
+HIGHEST_MPG = MPG.max()
+LOWEST_MPG = MPG.min()
+AVG_MPG = MPG.mean()
+COMMON_MPG = MPG.mode()
+
+# Print the highest MPG
+print('')
+print(':::HIGHEST MPG:::')
+print(HIGHEST_MPG)
+
+#Print the lowest MPG
+print('')
+print(':::LOWEST MPG:::')
+print(LOWEST_MPG)
+
+# Print the AVG MPG
+print('')
+print(':::AVERAGE MPG:::')
+print(AVG_MPG)
+
+# Print the most common MPG
+print('')
+print(':::MOST COMMON MPG:::')
+print(COMMON_MPG)
+
+#END
